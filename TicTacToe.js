3e0e86f463cd1866b21380b91a8268bcc9a1b0a3
Lukasz Pelczar <lukasxxz@o2.pl>
2022-02-17 14:52:30 +0100

    bigger game grid

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,13 +1,16 @@
 const board = document.querySelector('.board');
 
+
+
+
 let player = 'playerX';
 const winBoard = {
-  r0: [],
-  r1: [],
-  r2: [],
-  c0: [],
-  c1: [],
-  c2: [],
+  // r0: [],
+  // r1: [],
+  // r2: [],
+  // c0: [],
+  // c1: [],
+  // c2: [],
   playerXCross: {
     cIndex: [],
     rIndex: [],
@@ -20,6 +23,17 @@ const winBoard = {
 
 const winLength = 3;
 
+for (let i = 0; i < 5; i++) {
+  for (let j = 0; j < 5; j++) {
+    const elem = document.createElement('div');
+    elem.classList.add('box');
+    elem.setAttribute('data-row', i);
+    elem.setAttribute('data-column', j);
+    board.append(elem);
+  }
+  winBoard[`c${i}`] = [];
+  winBoard[`r${i}`] = [];
+}
 
 const win = (winner) => {
   console.log(winner);
@@ -40,10 +54,11 @@ const game = (e) => {
   const rValue = box.getAttribute('data-row');
 
   const coords = [`c${cValue}`, `r${rValue}`];
+  const winCrossOption = winBoard[`${player}Cross`];
 
   coords.forEach((coord) => {
     const winOption = winBoard[coord];
-    const winCrossOption = winBoard[`${player}Cross`];
+
     winOption.push(sign);
     switch (coord[0]) {
       case 'c':
@@ -55,33 +70,48 @@ const game = (e) => {
     }
 
     if (winOption.length === winLength && winOption.every(item => item === sign)) winner = player;
-    if (winCrossOption.cIndex.length >= winLength && winCrossOption.rIndex.length >= winLength && (winCrossOption.cIndex.length === winCrossOption.rIndex.length)) {
-      const rowIds = [...winCrossOption.rIndex];
-      const colIds = [...winCrossOption.cIndex];
+  });
 
-      for (let i = 0; i < winLength; i++) {
-        const partRow = [...new Set(rowIds.slice(i, i + 3))];
-        const partCol = [...new Set(colIds.slice(i, i + 3))];
+  if (winCrossOption.cIndex.length >= winLength && winCrossOption.rIndex.length >= winLength && (winCrossOption.cIndex.length === winCrossOption.rIndex.length)) {
+    const rowIds = [...new Set(winCrossOption.rIndex)].sort();
+    const colIds = [...new Set(winCrossOption.cIndex)].sort();
 
-        if (partRow.length === winLength && partCol.length === winLength) {
-          const rowIdSum = partRow.reduce((prev, curr) => prev + Number(curr), 0);
-          const colIdSum = partCol.reduce((prev, curr) => prev + Number(curr), 0);
+    for (let i = 0; i <= Math.floor(rowIds.length / winLength); i++) {
+      const partRow = rowIds.slice(i, i + winLength);
+      const partCol = colIds.slice(i, i + winLength);
 
-          if (rowIdSum === colIdSum) {
-            winner = player;
-            break;
-          }
+      let flagRow = [];
+      let flagCol = [];
+
+      if (partRow.length === winLength && partCol.length === winLength) {
+        partRow.reduce((prev, curr) => {
+          const currNum = Number(curr);
+          const prevNum = Number(prev);
+
+          (currNum - prevNum === 1 || currNum - prevNum === -1) ? flagRow.push(1) : flagRow.push(0);
+          return currNum;
+        });
+
+        partCol.reduce((prev, curr) => {
+          const currNum = Number(curr);
+          const prevNum = Number(prev);
+
+          (currNum - prevNum === 1 || currNum - prevNum === -1) ? flagCol.push(1) : flagCol.push(0);
+          return currNum;
+        });
+
+
+        if (flagCol.every(item => Number(item) === 1) && flagRow.every(item => Number(item) === 1)) {
+          winner = player;
+          break;
         }
-
       }
     }
-
-  });
+  }
 
   player = player === 'playerX' ? 'playerO' : 'playerX';
 
   if (winner) win(winner);
-
 }
 
 const handleGame = (e) => {
